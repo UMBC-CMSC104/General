c21a76c946b41f9a6258e12260b1039b47e0e91f
Michael Wilson <mwilson3@umbc.edu>
2012-11-09 02:22:52 -0500

    Addition of suite functions for the final.

---
+++ final/suite_functions.c
@@ -0,0 +1,77 @@
+/* File: suite_functions.c
+   Name: Michael Wilson
+   Username: mwilson3
+   Date: 11/9/2012
+   Description: This program demonstrates the get_suite and get_face_value 
+                functions.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+/* Returns a string representation of the suite for the card */
+char *get_suite(int card, char *suite)
+{
+    int suite_int = card / 13;
+
+    switch(suite_int)
+    {
+        case 0: strcpy(suite, "Spades"); break;
+        case 1: strcpy(suite, "Hearts"); break;
+        case 2: strcpy(suite, "Diamonds"); break;
+        case 3: strcpy(suite, "Clubs"); break;
+    }
+
+    return suite;
+}
+
+/* Returns a string representation of the face value for the card */
+char *get_face_value(int card, char *face_value)
+{
+    int face_value_number = (card - 1) % 13;
+
+    switch(face_value_number)
+    {
+        case 0: strcpy(face_value, "Ace"); break;
+        case 1: strcpy(face_value, "2"); break;
+        case 2: strcpy(face_value, "3"); break;
+        case 3: strcpy(face_value, "4"); break;
+        case 4: strcpy(face_value, "5"); break;
+        case 5: strcpy(face_value, "6"); break;
+        case 6: strcpy(face_value, "7"); break;
+        case 7: strcpy(face_value, "8"); break;
+        case 8: strcpy(face_value, "9"); break;
+        case 9: strcpy(face_value, "10"); break;
+        case 10: strcpy(face_value, "Jack"); break;
+        case 11: strcpy(face_value, "Queen"); break;
+        case 12: strcpy(face_value, "King"); break;
+    }
+
+    return face_value;
+}
+
+/* Main function */
+int main()
+{
+    char suite[16];
+    char face_value[16];
+
+    int card1 = 12, card2 = 33, card3 = 25;
+
+    /* Card 1 */
+    get_face_value(card1, face_value);
+    get_suite(card1, suite);
+    printf("%s of %s\n", face_value, suite);
+
+    /* Card 2 */
+    get_face_value(card2, face_value);
+    get_suite(card2, suite);
+    printf("%s of %s\n", face_value, suite);
+
+    /* Card 3 */
+    get_face_value(card3, face_value);
+    get_suite(card3, suite);
+    printf("%s of %s\n", face_value, suite);
+
+    return 0;
+}
